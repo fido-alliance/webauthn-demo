64a5a46fa245e369241611502daddf5cc0543867
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2018-01-08 21:31:25 +0000

    Completed register and skeleton for response webauthn routes

--- routes/webauthn.js
+++ routes/webauthn.js
@@ -3,4 +3,60 @@ const utils    = require('../utils');
 const router   = express.Router();
 const database = require('./db');
 
+router.post('/register', (request, response) => {
+    if(!request.body || !request.body.username || !request.body.name) {
+        response.json({
+            'status': 'failed',
+            'message': 'Request missing name or username failed!'
+        })
+
+        return
+    }
+
+    let username = request.body.username;
+    let name     = request.body.name;
+
+    if(database[username] && database[username].registered) {
+        response.json({
+            'status': 'failed',
+            'message': `Username ${username} already exists`
+        })
+
+        return
+    }
+
+    database[username] = {
+        'name': name,
+        'registered': false,
+        'id': utils.randomBase64URLBuffer()
+    }
+
+    let challengeMakeCred = utils.generateServerMakeCredRequest(username, name, database[username].id)
+    challengeMakeCred.status = 'ok'
+
+    request.session.challenge = challengeMakeCred.challenge;
+    request.session.username  = username;
+
+    response.json(challengeMakeCred)
+})
+
+router.post('/response', (request, response) => {
+    if(!request.body || !request.body.id 
+    || !request.body.rawId || !request.body.response
+    || !request.body.type || request.body.type !== 'public-key' ) {
+        response.json({
+            'status': 'failed',
+            'message': 'Response missing one or more of id/rawId/response/type fields, or type is not public-key!'
+        })
+
+        return
+    }
+
+    console.log(request.body)
+
+    response.json({
+        'status': 'ok'
+    })
+})
+
 module.exports = router;
