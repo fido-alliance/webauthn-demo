6c717447070156124b5a02b1093cfdd6683dc932
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2018-01-11 12:52:53 +0000

    Added webauthn login route. Completed response route

--- routes/webauthn.js
+++ routes/webauthn.js
@@ -1,13 +1,15 @@
-const express  = require('express');
-const utils    = require('../utils');
-const router   = express.Router();
-const database = require('./db');
+const express   = require('express');
+const utils     = require('../utils');
+const config    = require('../config.json');
+const base64url = require('base64url');
+const router    = express.Router();
+const database  = require('./db');
 
 router.post('/register', (request, response) => {
     if(!request.body || !request.body.username || !request.body.name) {
         response.json({
             'status': 'failed',
-            'message': 'Request missing name or username failed!'
+            'message': 'Request missing name or username field!'
         })
 
         return
@@ -31,7 +33,7 @@ router.post('/register', (request, response) => {
         'id': utils.randomBase64URLBuffer()
     }
 
-    let challengeMakeCred = utils.generateServerMakeCredRequest(username, name, database[username].id)
+    let challengeMakeCred    = utils.generateServerMakeCredRequest(username, name, database[username].id)
     challengeMakeCred.status = 'ok'
 
     request.session.challenge = challengeMakeCred.challenge;
@@ -40,10 +42,40 @@ router.post('/register', (request, response) => {
     response.json(challengeMakeCred)
 })
 
+router.post('/login', (request, response) => {
+    if(!request.body || !request.body.username) {
+        response.json({
+            'status': 'failed',
+            'message': 'Request missing username field!'
+        })
+
+        return
+    }
+
+    let username = request.body.username;
+
+    if(!database[username] || !database[username].registered) {
+        response.json({
+            'status': 'failed',
+            'message': `Username ${username} already does not exist!`
+        })
+
+        return
+    }
+
+    let getAssertion    = utils.generateServerGetAssertion(database[username].id)
+    getAssertion.status = 'ok'
+
+    request.session.challenge = getAssertion.challenge;
+    request.session.username  = username;
+
+    response.json(getAssertion)
+})
+
 router.post('/response', (request, response) => {
-    if(!request.body || !request.body.id 
+    if(!request.body       || !request.body.id
     || !request.body.rawId || !request.body.response
-    || !request.body.type || request.body.type !== 'public-key' ) {
+    || !request.body.type  || request.body.type !== 'public-key' ) {
         response.json({
             'status': 'failed',
             'message': 'Response missing one or more of id/rawId/response/type fields, or type is not public-key!'
@@ -52,10 +84,50 @@ router.post('/response', (request, response) => {
         return
     }
 
-    console.log(request.body)
+    let webauthnResp = request.body
+    let clientData   = JSON.parse(base64url.decode(webauthnResp.response.clientDataJSON));
+
+    /* Check challenge... */
+    if(clientData.challenge !== request.session.challenge) {
+        response.json({
+            'status': 'failed',
+            'message': 'Challenges don\'t match!'
+        })
+    }
+
+    /* ...and origin */
+    if(clientData.origin !== config.origin) {
+        response.json({
+            'status': 'failed',
+            'message': 'Origins don\'t match!'
+        })
+    }
+
+    let result = false;
+    if(webauthnResp.attestationObject !== undefined) {
+        /* This is create cred */
+        result = utils.verifyAuthenticatorAttestationResponse(webauthnResp);
+
+        if(result) {
+            database[request.session.username].registered = true;
+            database[request.session.username].publicKey  = utils.extractPublicKey(webauthnResp);
+        }
+
+    } else if(webauthnResp.authenticatorData !== undefined) {
+        /* This is get assertion */
+        result = utils.verifyAuthenticatorAssertionResponse(webauthnResp, database[request.session.username].publicKey);
+    }
+
+    if(result) {
+        request.session.loggedIn = true;
+        response.json({
+            'status': 'ok'
+        })
+    }
 
     response.json({
-        'status': 'ok'
+        'status': 'failed',
+        'message': 'Can not determine type of response!'
     })
 })
 
