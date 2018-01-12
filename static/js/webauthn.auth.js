4d913d8dcc4ea103be9fbcb585178fd32d4094e0
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2018-01-12 09:31:34 +0000

    Implemented login

--- static/js/webauthn.auth.js
+++ static/js/webauthn.auth.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let getMakeCredChallenge = (formBody) => {
+let getMakeCredentialsChallenge = (formBody) => {
     return fetch('/webauthn/register', {
         method: 'POST',
         credentials: 'include',
@@ -44,18 +44,73 @@ $('#register').submit(function(event) {
     let name     = this.name.value;
 
     if(!username || !name) {
-        alert('Name, username or password is missing!')
+        alert('Name or username is missing!')
         return
     }
 
-    getMakeCredChallenge({username, name})
+    getMakeCredentialsChallenge({username, name})
         .then((response) => {
             let publicKey = preformatMakeCredReq(response);
             return navigator.credentials.create({ publicKey })
         })
         .then((response) => {
-            let publicKeyCredentialResponse = publicKeyCredentialToJSON(response);
-            return sendWebAuthnResponse(publicKeyCredentialResponse)
+            let makeCredResponse = publicKeyCredentialToJSON(response);
+            return sendWebAuthnResponse(makeCredResponse)
+        })
+        .then((response) => {
+            if(response.status === 'ok') {
+                loadMainContainer()   
+            } else {
+                alert(`Server responed with error. The message is: ${response.message}`);
+            }
+        })
+        .catch((error) => alert(error))
+})
+
+let getGetAssertionChallenge = (formBody) => {
+    return fetch('/webauthn/login', {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formBody)
+    })
+    .then((response) => response.json())
+    .then((response) => {
+        if(response.status !== 'ok')
+            throw new Error(`Server responed with error. The message is: ${response.message}`);
+
+        return response
+    })
+}
+
+/* Handle for login form submission */
+$('#login').submit(function(event) {
+    event.preventDefault();
+
+    let username = this.username.value;
+
+    if(!username) {
+        alert('Username is missing!')
+        return
+    }
+
+    getGetAssertionChallenge({username, name})
+        .then((response) => {
+            let publicKey = preformatGetAssertReq(response);
+            return navigator.credentials.get({ publicKey })
+        })
+        .then((response) => {
+            let getAssertionResponse = publicKeyCredentialToJSON(response);
+            return sendWebAuthnResponse(getAssertionResponse)
+        })
+        .then((response) => {
+            if(response.status === 'ok') {
+                loadMainContainer()   
+            } else {
+                alert(`Server responed with error. The message is: ${response.message}`);
+            }
         })
         .catch((error) => alert(error))
 })
