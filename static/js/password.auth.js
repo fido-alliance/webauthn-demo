72ce94c1a6b6a1bc80c23bf87467e22396a186f7
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2018-04-18 10:35:39 +0300

    Commented password auth

--- static/js/password.auth.js
+++ static/js/password.auth.js
@@ -1,65 +1,65 @@
-/* Handle for register form submission */
-$('#register').submit(function(event) {
-    event.preventDefault();
+// /* Handle for register form submission */
+// $('#register').submit(function(event) {
+//     event.preventDefault();
 
-    let username = this.username.value;
-    let password = this.password.value;
-    let name     = this.name.value;
+//     let username = this.username.value;
+//     let password = this.password.value;
+//     let name     = this.name.value;
 
-    if(!username || !password || !name) {
-        alert('Name, username or password is missing!')
-        return
-    }
+//     if(!username || !password || !name) {
+//         alert('Name, username or password is missing!')
+//         return
+//     }
 
-    let formBody = {username, password, name}; 
+//     let formBody = {username, password, name}; 
 
-    fetch('/password/register', {
-        method: 'POST',
-        credentials: 'include',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(formBody)
-    })
-    .then((response) => response.json())
-    .then((response) => {
-        if(response.status === 'ok') {
-            loadMainContainer()
-        } else {
-            alert(`Server responed with error. The message is: ${response.message}`);
-        }
-    })
-})
+//     fetch('/password/register', {
+//         method: 'POST',
+//         credentials: 'include',
+//         headers: {
+//           'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(formBody)
+//     })
+//     .then((response) => response.json())
+//     .then((response) => {
+//         if(response.status === 'ok') {
+//             loadMainContainer()
+//         } else {
+//             alert(`Server responed with error. The message is: ${response.message}`);
+//         }
+//     })
+// })
 
-/* Handle for login form submission */
-$('#login').submit(function(event) {
-    event.preventDefault();
+// /* Handle for login form submission */
+// $('#login').submit(function(event) {
+//     event.preventDefault();
 
-    let username = this.username.value;
-    let password = this.password.value;
+//     let username = this.username.value;
+//     let password = this.password.value;
 
-    if(!username || !password) {
-        alert('Username or password is missing!')
-        return
-    }
+//     if(!username || !password) {
+//         alert('Username or password is missing!')
+//         return
+//     }
 
-    let formBody = {username, password}; 
-    fetch('/password/login', {
-        method: 'POST',
-        credentials: 'include',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(formBody)
-    })
-    .then((response) => response.json())
-    .then((response) => {
-        if(response.status === 'ok') {
-            loadMainContainer()   
-        } else {
-            alert(`Server responed with error. The message is: ${response.message}`);
-        }
-    })
-})
+//     let formBody = {username, password}; 
+//     fetch('/password/login', {
+//         method: 'POST',
+//         credentials: 'include',
+//         headers: {
+//           'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(formBody)
+//     })
+//     .then((response) => response.json())
+//     .then((response) => {
+//         if(response.status === 'ok') {
+//             loadMainContainer()   
+//         } else {
+//             alert(`Server responed with error. The message is: ${response.message}`);
+//         }
+//     })
+// })
 
 
